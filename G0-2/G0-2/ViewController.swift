3fed8c8a57db97139143f9ef58802deafc85d86a
harnam1 <63170760+harnam1@users.noreply.github.com>
2020-06-09 01:11:28 +0100

    Add files via upload

---
+++ G0-2/G0-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  G0-2
+//
+//  Created by Harnam Singh Thethi on 08/06/2020.
+//  Copyright © 2020 Harnam Singh Thethi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
